25b95f47027f18e413fd87a105ded5e733be3b04
jankos <janis.koselevs@gmail.com>
2014-03-28 10:57:17 +0000

    Adding single post and page templates

---
+++ templates/single.php
@@ -0,0 +1,27 @@
+        <div class="row row-abcd-ef">
+            <div class="col col-abcd">
+                <div class="cell" id="page">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div>
+                    <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+                    <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+                    
+                    <div class="entry">
+                    <?php the_content(); ?>
+                    </div>
+                    
+                    <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
+                    </div>
+                    <?php endwhile; else: ?>
+                    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="col col-ef">
+                <div class="cell" id="sidebar">
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar Widgets') ) : ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
