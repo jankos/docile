00a3390450e685416b0d6f878cf03a97892e8984
jankos <janis.koselevs@gmail.com>
2014-05-26 19:05:28 +0000

    Commiting latest changes/files

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
 get_header();
 get_template_part( 'templates/single' );
+comments_template();
 get_footer();
--- templates/single.php
+++ templates/single.php
@@ -1,8 +1,7 @@
         <div class="row row-abcd-ef">
             <div class="col col-abcd">
-                <div class="cell" id="page">
+                <div class="cell" id="article">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div>
                     <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
                     <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
@@ -17,6 +16,7 @@
                     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
                     <?php endif; ?>
                 </div>
+                <?php  ?>
             </div>
             <div class="col col-ef">
                 <div class="cell" id="sidebar">
