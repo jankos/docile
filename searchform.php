50932c04598c2382bc285bcdfe87841b05eef670
jankos <janis.koselevs@gmail.com>
2014-09-09 00:40:58 +0300

    Removing search form template

--- searchform.php
+++
@@ -1,9 +0,0 @@
-<?php if (is_search()) { $search_text = "$s"; } ?>
-<div id="search">
-    <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-        <div>
-            <input type="text" name="s" id="s" placeholder="search" />
-            <button type="submit">search</button>
-        </div>
-    </form>
-</div>
