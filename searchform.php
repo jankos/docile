c47d3f9deefe5d950e6da127d0c2b732182e8400
jankos <janis.koselevs@gmail.com>
2014-09-08 21:48:08 +0300

    Updating searchform

--- searchform.php
+++ searchform.php
@@ -1,12 +1,9 @@
-<?php if (!is_search()) { $search_text = '';} else { $search_text = "$s"; } ?>
+<?php if (is_search()) { $search_text = "$s"; } ?>
 <div id="search">
     <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
         <div>
-            <input type="text" name="s" id="s"
-                   value="<?php echo wp_specialchars($search_text, 1); ?>"
-                   onFocus="clearInput('s', '<?php echo wp_specialchars($search_text, 1); ?>')"
-                   onBlur="clearInput('s', '<?php echo wp_specialchars($search_text, 1); ?>')"
-                   />
+            <input type="text" name="s" id="s" placeholder="search" />
+            <button type="submit">search</button>
         </div>
     </form>
 </div>
